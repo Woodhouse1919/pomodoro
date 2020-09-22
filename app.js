79807e34c4569870c694e14ae96cc994eafd404f
Woodhouse <pariswoodhouse@hotmail.com>
2020-09-21 20:48:52 -0700

    Fix timer reset

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const startBtn = document.getElementById('startButton')
 const pomodoroBtn = document.getElementById('pomodoro')
 const shortBreakBtn = document.getElementById('shortBreak')
 const longBreakBtn = document.getElementById('longBreak')
-pomodoroBtn.style.backgroundColor = '#ce5c58'
+buttonSettings()
 
 
 startBtn.addEventListener('click', () => {
@@ -14,7 +14,43 @@ startBtn.addEventListener('click', () => {
     } 
 })
 
+function buttonSettings() {
+pomodoroBtn.addEventListener('click', () => {
+    shortBreakBtn.style.backgroundColor = 'transparent'
+    longBreakBtn.style.backgroundColor = 'transparent'
+    pomodoroBtn.style.backgroundColor = '#ce5c58'
+    timer.innerHTML = '25:00'
+    
+})
+shortBreakBtn.addEventListener('click', () => {
+    pomodoroBtn.style.backgroundColor = 'transparent'
+    longBreakBtn.style.backgroundColor = 'transparent'
+    shortBreakBtn.style.backgroundColor = '#ce5c58'
+    timer.innerHTML = '5:00'
+})
+longBreakBtn.addEventListener('click', () => {
+    pomodoroBtn.style.backgroundColor = 'transparent'
+    shortBreakBtn.style.backgroundColor = 'transparent'
+    longBreakBtn.style.backgroundColor = '#ce5c58'
+    timer.innerHTML = '10:00'
+})
+}
+const startingMinutes = 25;
+let time = startingMinutes * 60;
 
+
+
+setInterval(countdown, 1000)
+
+function countdown() {
+    
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    timer.innerHTML = `${minutes}:${seconds}`
+    time--;
+}
 
 
 
